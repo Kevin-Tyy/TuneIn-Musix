e59959a5e29771b33297a293bd78e0fd7cc0f21a
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-06-23 20:33:37 +0200

    Setting up tailwindcss boilerplate

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,tsx,jsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
