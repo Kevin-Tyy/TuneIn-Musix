904328a66b4973dca334878afb29f4fa79f9d6f9
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-08-12 23:35:05 +0200

    Formatting timestamps with momment

--- client/src/components/navigation/navBar/components/ProfileModal.tsx
+++ client/src/components/navigation/navBar/components/ProfileModal.tsx
@@ -2,6 +2,12 @@ import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import { MdClose } from "react-icons/md";
 
+const settings = [
+	{ title: "Profile", link: "#" },
+	{ title: "Your playlists", link: "#" },
+	{ title: "Your activity", link: "#" },
+	{ title: "Logout", link: "#" },
+];
 const ProfileModal = ({ isOpen, onClose }: any) => {
 	return (
 		<Transition.Root show={isOpen} as={Fragment}>
@@ -13,18 +19,14 @@ const ProfileModal = ({ isOpen, onClose }: any) => {
 				leave="ease-in duration-200"
 				leaveFrom="opacity-100 translate-y-0 sm:scale-100"
 				leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-				<div className="absolute right-0 overflow-hidden rounded-lg bg-white px-4  pb-4  pt-5 text-left shadow-xl transition-all w-full sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-					<button onClick={onClose}>
-						<MdClose />
+				<div className="absolute right-0 overflow-hidden rounded-lg  bg-primary-dark px-1  pb-4  pt-5 text-left shadow-xl transition-all w-full sm:my-5 sm:w-full sm:max-w-lg">
+					<button onClick={onClose} className="absolute top-1 right-1 p-1 rounded-md text-gray-400 hover:bg-gray-600 hover:text-white transition">
+						<MdClose size={25}/>
 					</button>
-					<ul className="name is-open">
-						<li>hi</li>
-						<li>hi</li>
-						<li>hi</li>
-						<li>hi</li>
-						<li>hi</li>
-						<li>hi</li>
-						<li>hi</li>
+					<ul className="space-y-2 mt-2">
+						{settings.map((item, index) => (
+							<li key={index} className="p-2 hover:bg-primary-500">{item.title}</li>
+						))}
 					</ul>
 				</div>
 			</Transition.Child>
