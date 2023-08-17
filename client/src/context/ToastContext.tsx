4bb4e02eb62e792832ececff91ac211b6059d6a4
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-08-17 20:49:45 +0200

    toast component context

--- client/src/context/ToastContext.tsx
+++ client/src/context/ToastContext.tsx
@@ -9,11 +9,8 @@ const ToasterContext: FC<ToastContextProps> = ({ children }) => {
 			<Toaster
 				toastOptions={{
 					style: {
-						backgroundColor: "#340F5F6d",
-						border: "1px solid #340F5F",
-						color: "white",
 						fontSize: "14px",
-						padding : '15px',
+						padding : '13px',
 					},
 				}}
 			/>
