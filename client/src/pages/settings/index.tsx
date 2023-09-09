0b4e314e145f339d434bbc858bb846dccf51291f
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-09-09 02:29:04 +0200

    Drag and drop to add playlists

--- client/src/pages/settings/index.tsx
+++ client/src/pages/settings/index.tsx
@@ -1,22 +1,9 @@
 import React from "react";
-import Playlist from "./Playlist";
-import Track from "./Track";
 
 const SettingsPage: React.FC = () => {
 	return (
 		<div>
-			<h1>music list (drag and drop to add music to your playlist)</h1>
-			<Track id="music1" title="Demo Sagamba" />
-			<Track id="music2" title="Joli Kenny Sol" />
-			<Track id="music3" title="Fenty Kenny Sol 2 saint" />
-			<Track id="music4" title="Bandana Fireboy Asake DML " />
-			<Track id="music5" title="Bloody Samaritan Ayra Starr" />
-
-			<div className="mt-10">
-				<h1>Your playlists</h1>
-				<Playlist />
-			</div>
-			dlkahsd
+			<h1>Settings</h1>
 		</div>
 	);
 };
